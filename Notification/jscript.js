97b707c72017829f46238a26c21a19d70d77aac3
Aditya_Choudhry <63087556+Miki-Official@users.noreply.github.com>
2022-03-20 16:04:00 +0530

    Update jscript.js

--- Notification/jscript.js
+++ Notification/jscript.js
@@ -1,7 +1,19 @@
 const btn = document.getElementById("btn");
-const nav = document.getElementById("nav");
+const container = document.getElementById("container");
 
 btn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    btn.classList.toggle("active");
+    createNotification();
 });
+
+function createNotification() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText = " Mr.Aditya is best developer";
+
+    container.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 3000);
+}
